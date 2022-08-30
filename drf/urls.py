829fdec0dbe79e613c4796ed26f2d6bcdeabf0fa
RIKI PLAZA <rikiplaza1@gmail.com>
2022-08-30 08:04:52 -0400

    added docker and started repo over

---
+++ drf/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ThingList, ThingDetail
+
+urlpatterns = [
+    path("", ThingList.as_view(), name="thing_list"),
+    path("<int:pk>/", ThingDetail.as_view(), name="thing_detail"),
+]
+Footer
+© 2022 GitHub, Inc.
